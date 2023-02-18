99c89a8a25465e886b298b1df610c41eedcf9d71
Edu <eduardo.ex7@gmail.com>
2023-02-18 11:24:11 -0300

    Alt. 1

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.texto}>Edu Aqui</Text>
     </View>
   );
 }
@@ -17,4 +17,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  texto:{
+    fontSize: 20,
+    fontWeight: "bold"
+  }
 });
